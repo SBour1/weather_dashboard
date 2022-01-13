14581d31036e0ff317c6d7c605f606cce7a19ca3
Steve Bour <bourpower@gmail.com>
2022-01-12 21:36:27 -0500

    completed 5 day forecast and main card

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,12 +12,10 @@ function weatherCall(city) {
     var city = $('#user-input').val()
     // searchHistoryBtn = $('<button class="newSearch">' + localStorage.getItem(city, city) + '</button>')
     // confirmBtn = document.querySelector('.newSearch')
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + weatherKey)
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + weatherKey + '&units=imperial')
         .then(data => data.json())
         .then(function (response) {
-            console.log(response)
-            var farTemp = ((response.main.temp - 273.15) * 1.8) + 32
-            tempEl.html("Temperature: " + Math.trunc(farTemp) + '&#176');
+            tempEl.html("Temperature: " + Math.trunc(response.main.temp) + '&#176');
             humidityEl.html("Humidity: " + response.main.humidity + "%");
             windEl.html("Wind Speed: " + response.wind.speed + " MPH");
             var currentDate = new Date(response.dt * 1000);
@@ -30,49 +28,44 @@ function weatherCall(city) {
             picEl.attr("alt", response.weather[0].description);
             var lat = response.coord.lat
             var lon = response.coord.lon
-            console.log(lat, lon)
-            // fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon= ' + lon + '&appid=' + weatherKey)
-            //     .then(uv => uv.json())
-            //     .then(function (response) {
-            //         console.log(response)
-            //     })
-            fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + weatherKey)
+            fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=' + weatherKey)
+                .then(uv => uv.json())
+                .then(function (response) {
+                    uvEl.html("UV: " + response.daily[0].uvi)
+                    if (response.daily[0].uvi > 5) {
+                        uvEl.attr("style", "background-color: red; display: inline")
+                    } else if (response.daily[0].uvi <= 2) {
+                        uvEl.attr("style", "background-color: green; display: inline")
+                    } else {
+                        uvEl.attr("style", "background-color: yellow; display: inline")
+                    }
+                })
+            fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + weatherKey + '&units=imperial')
                 .then(fiveDay => fiveDay.json())
                 .then(response => {
-                    console.log(response)
                     for (let i = 0; i < forecastEl.length; i++) {
                         var forecastIndex = i * 8 + 4
                         var forecastDate = new Date(response.list[forecastIndex].dt * 1000)
                         var forecastDay = forecastDate.getDate();
                         var forecastMonth = forecastDate.getMonth() + 1;
                         var forecastYear = forecastDate.getFullYear();
-                        $('.forecast').append('<h4>').html(forecastMonth + "/" + forecastDay + "/" + forecastYear)
-                        var forecastPic = $('.forecast').append('<img>');
-                        forecastPic.attr("src", "https://openweathermap.org/img/wn/" + response.list[forecastIndex].weather[0].icon + "@2x.png");
-                        forecastPic.attr("alt", response.list[forecastIndex].weather[0].description);
-                        var forecastTempEl = forecastPic.append('<p>')
-                        var forecastTemp = ((response.list[forecastIndex].main.temp - 273.15) * 1.8) + 32
-                        var forecastHumidity = forecastTempEl.append('<p>')
-                        forecastTempEl.html('Temp: ' + Math.trunc(forecastTemp) + '&#176')
-                        console.log(response.list[forecastIndex].main.humidity)
-                        forecastHumidity.html('Humidity: ' + response.list[forecastIndex].main.humidity + '%')
+                        $('#' + i).append('<h4>').html(forecastMonth + "/" + forecastDay + "/" + forecastYear)
+                        $('#' + i).append('<img src="https://openweathermap.org/img/wn/' + response.list[forecastIndex].weather[0].icon + '@2x.png"/>');
+                        $('#' + i).append('<p> Temp: ' + response.list[forecastIndex].main.temp + '&#176</p>')
+                        $('#' + i).append('<p> Humidity: ' + response.list[forecastIndex].main.humidity + '%</p>')
+                        $('#' + i).append('<p> Wind: ' + response.list[forecastIndex].wind.speed + " MPH</p>")
                     }
                 })
         })
-
-
-    //     localStorage.setItem(city, city)
-    //     if (confirmBtn){
-    //         confirmBtn.remove();
-    //     } else {      
-    //         for (let i = 0; i < localStorage.length; i++) {
-    //             $('.input-group').after($('<button class="newSearch">' + localStorage.getItem(localStorage[i]) + '</button>'))
-    //         }
-    //     }
-    //     $('.newSearch').click(function (){
-    //         console.log("city")
-    //         weatherCall($('.newSearch').text())
-    //     })
+    localStorage.setItem(city, city)
+    // for (let i = 0; i < localStorage.length; i++) {
+    //     $('.input-group').after($('<button class="newSearch">' + localStorage.getItem(localStorage.key(i)) + '</button>'))
+    //     console.log($('.newSearch').text())
+    // }
 }
+// $('.newSearch').click(function () {
+//     console.log("city")
+//     weatherCall($('.newSearch').text())
+// })
 
 search.click(weatherCall) 
